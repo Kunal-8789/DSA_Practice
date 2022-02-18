02902f5da0366c11692ab84863481460225dd606
Kunal-8789 <mrblazerebel@gmail.com>
2022-02-18 15:17:32 +0530

    LCS(substirng)😎

---
+++ Leetcode/718.maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=718 lang=cpp
+ *
+ * [718] Maximum Length of Repeated Subarray
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int findLength(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n1 = nums1.size(), n2 = nums2.size();
+        int t[n1 + 1][n2 + 1];
+        for (int i = 0; i <= n1; i++)
+        {
+            for (int j = 0; j <= n2; j++)
+            {
+                if (i == 0 or j == 0)
+                {
+                    t[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i <= n1; i++)
+        {
+            for (int j = 1; j <= n2; j++)
+            {
+                if (nums1[i-1]==nums2[j-1])
+                {
+                    t[i][j]= 1+ t[i-1][j-1];
+                }
+                else
+                {
+                    t[i][j] = 0;
+                }
+                
+            }
+        }
+        int ans=INT_MIN;
+        for (int i = 0; i <= n1; i++)
+        {
+            for (int j = 0; j <= n2; j++)
+            {
+                ans=max(ans,t[i][j]);
+            }
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
